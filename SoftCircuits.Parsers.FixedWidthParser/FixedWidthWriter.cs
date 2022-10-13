a665fa081ed0d911d0405b7473d3f56dbe4ec8c9
Peter Maughan <peter.maughan@accessbookings.com>
2022-10-13 12:13:26 +0100

    Added option to pass NewLine via the options so that you can customise what NewLine characters you would use with the textWriter

--- SoftCircuits.Parsers.FixedWidthParser/FixedWidthWriter.cs
+++ SoftCircuits.Parsers.FixedWidthParser/FixedWidthWriter.cs
@@ -2,220 +2,79 @@
 // Licensed under the MIT license.
 //
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SoftCircuits.Parsers
 {
     /// <summary>
-    /// Class to write fixed-width fields to a file.
+    /// Specifies options used by the <see cref="FixedWidthReader"/> and
+    /// <see cref="FixedWidthWriter"/> classes.
     /// </summary>
-    public class FixedWidthWriter : IDisposable
+    public class FixedWidthOptions
     {
-        private readonly StreamWriter Writer;
-        protected readonly FixedWidthOptions Options;
+        /// <summary>
+        /// Gets or sets the default way fields are padded. For example, if a field is right aligned, values shorter
+        /// than the field width are padded on the left. Can be overridden for individual fields using the
+        /// <see cref="FixedWidthField.Alignment"/> property. The default value is
+        /// <see cref="FieldAlignment.Left"/>.
+        /// </summary>
+        public FieldAlignment DefaultAlignment { get; set; }
 
         /// <summary>
-        /// Gets the list of fixed-width field descriptors.
+        /// Gets or sets the default character used to pad fields when writing values shorter than the field width.
+        /// Can be overridden for individual fields using the <see cref="FixedWidthField.PadCharacter"/> property.
+        /// The default value is <c>' '</c>.
         /// </summary>
-        public List<FixedWidthField> Fields { get; private set; }
+        public char DefaultPadCharacter { get; set; }
 
         /// <summary>
-        /// Constructs a new <see cref="FixedWidthWriter"/> instance.
+        /// <para>
+        /// Gets or sets whether leading and trailing pad characters are trimmed when reading field values. Can be
+        /// overridden for individual fields using the <see cref="FixedWidthField.TrimField"/> property. The default
+        /// value is <c>true</c>.
+        /// </para>
+        /// <para>
+        /// WARNING: If this property is <c>true</c> and the field value contains leading or trailing characters that
+        /// match the current pad character, those characters will also be trimmed.
+        /// </para>
         /// </summary>
-        /// <param name="fields">List of fixed-width field descriptors.</param>
-        /// <param name="filename">File to write.</param>
-        /// <param name="options">Library options. Leave as null to use the default options.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public FixedWidthWriter(IEnumerable<FixedWidthField> fields, string filename, FixedWidthOptions? options = null)
+        public bool TrimFields { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether a <see cref="FixedWidthDataException"/> exception is thrown when
+        /// reading a field that cannot be converted to the target field type. The default
+        /// value is <c>true</c>.
+        /// </summary>
+        public bool ThrowDataException;
+
+        /// <summary>
+        /// Gets or sets whether a <see cref="FixedWidthOutOfRangeException"/> exception is thrown when
+        /// reading a field from a line that is too short. If <c>false</c>, the library reads
+        /// as much of the field as possible or returns an empty string. The default value is
+        /// <c>true</c>.
+        /// </summary>
+        public bool ThrowOutOfRangeException;
+
+        /// <summary>
+        /// Gets or sets whether a <see cref="FixedWidthOverflowException"/> exception is thrown when
+        /// attempting to write a value that is too large for the field. If <c>false</c>,
+        /// the value will be silently truncated. The default value is <c>true</c>.
+        /// </summary>
+        public bool ThrowOverflowException;
+
+        public string NewLine;
+
+        /// <summary>
+        /// Constructs a new <see cref="FixedWidthOptions"/> instance.
+        /// </summary>
+        public FixedWidthOptions()
         {
-            if (fields == null)
-                throw new ArgumentNullException(nameof(fields));
-            if (filename == null)
-                throw new ArgumentNullException(nameof(filename));
-
-            Fields = new List<FixedWidthField>(fields);
-            Writer = new StreamWriter(filename);
-            Options = options ?? new FixedWidthOptions();
-        }
-
-        /// <summary>
-        /// Constructs a new <see cref="FixedWidthWriter"/> instance.
-        /// </summary>
-        /// <param name="fields">List of fixed-width field descriptors.</param>
-        /// <param name="filename">File to write.</param>
-        /// <param name="encoding">The character encoding to use.</param>
-        /// <param name="options">Library options. Leave as null to use the default options.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public FixedWidthWriter(IEnumerable<FixedWidthField> fields, string filename, Encoding encoding, FixedWidthOptions? options = null)
-        {
-            if (fields == null)
-                throw new ArgumentNullException(nameof(fields));
-            if (filename == null)
-                throw new ArgumentNullException(nameof(filename));
-            if (encoding == null)
-                throw new ArgumentNullException(nameof(encoding));
-
-            Fields = new List<FixedWidthField>(fields);
-            Writer = new StreamWriter(filename, false, encoding);
-            Options = options ?? new FixedWidthOptions();
-        }
-
-        /// <summary>
-        /// Constructs a new <see cref="FixedWidthWriter"/> instance.
-        /// </summary>
-        /// <param name="fields">List of fixed-width field descriptors.</param>
-        /// <param name="stream">Stream to write.</param>
-        /// <param name="options">Library options. Leave as null to use the default options.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public FixedWidthWriter(IEnumerable<FixedWidthField> fields, Stream stream, FixedWidthOptions? options = null)
-        {
-            if (fields == null)
-                throw new ArgumentNullException(nameof(fields));
-            if (stream == null)
-                throw new ArgumentNullException(nameof(stream));
-
-            Fields = new List<FixedWidthField>(fields);
-            Writer = new StreamWriter(stream);
-            Options = options ?? new FixedWidthOptions();
-        }
-
-        /// <summary>
-        /// Constructs a new <see cref="FixedWidthWriter"/> instance.
-        /// </summary>
-        /// <param name="fields">List of fixed-width field descriptors.</param>
-        /// <param name="stream">Stream to write.</param>
-        /// <param name="encoding">The character encoding to use.</param>
-        /// <param name="options">Library options. Leave as null to use the default options.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public FixedWidthWriter(IEnumerable<FixedWidthField> fields, Stream stream, Encoding encoding, FixedWidthOptions? options = null)
-        {
-            if (fields == null)
-                throw new ArgumentNullException(nameof(fields));
-            if (stream == null)
-                throw new ArgumentNullException(nameof(stream));
-            if (encoding == null)
-                throw new ArgumentNullException(nameof(encoding));
-
-            Fields = new List<FixedWidthField>(fields);
-            Writer = new StreamWriter(stream, encoding);
-            Options = options ?? new FixedWidthOptions();
-        }
-
-        /// <summary>
-        /// Writes a collection of fields to one line in the output file.
-        /// </summary>
-        /// <param name="args">The field values to write.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="FixedWidthOverflowException"></exception>
-        public void Write(params string[] args) => Write(args as IEnumerable<string>);
-
-        /// <summary>
-        /// Asynchronously writes a collection of fields to one line in the output file.
-        /// </summary>
-        /// <param name="args">The field values to write.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="FixedWidthOverflowException"></exception>
-        public async Task WriteAsync(params string[] args) => await WriteAsync(args as IEnumerable<string>);
-
-        /// <summary>
-        /// Writes a collection of fields to one line in the output file.
-        /// </summary>
-        /// <param name="values">The field values to write.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="FixedWidthOverflowException"></exception>
-        public void Write(IEnumerable<string>? values)
-        {
-            if (values == null)
-                return;
-
-            // Write fields to file
-            IEnumerator<string> enumerator = values.GetEnumerator();
-            foreach (FixedWidthField field in Fields)
-            {
-                if (field.Skip > 0)
-                    Writer.Write(new string(Options.DefaultPadCharacter, field.Skip));
-                string value = enumerator.MoveNext() ? enumerator.Current : string.Empty;
-                Writer.Write(FormatField(value, field));
-            }
-            Writer.WriteLine();
-        }
-
-        /// <summary>
-        /// Asynchronously writes a collection of fields to one line in the output file.
-        /// </summary>
-        /// <param name="values">The field values to write.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="FixedWidthOverflowException"></exception>
-        public async Task WriteAsync(IEnumerable<string>? values)
-        {
-            if (values == null)
-                return;
-
-            // Write fields to file
-            IEnumerator<string> enumerator = values.GetEnumerator();
-            foreach (FixedWidthField field in Fields)
-            {
-                if (field.Skip > 0)
-                    Writer.Write(new string(Options.DefaultPadCharacter, field.Skip));
-                string value = enumerator.MoveNext() ? enumerator.Current : string.Empty;
-                await Writer.WriteAsync(FormatField(value, field));
-            }
-            await Writer.WriteLineAsync();
-        }
-
-        /// <summary>
-        /// Formats a single field for writing.
-        /// </summary>
-        /// <param name="value">The value to format.</param>
-        /// <param name="field">The descriptor for the current field.</param>
-        /// <returns>The formatted string.</returns>
-        private string FormatField(string value, FixedWidthField field)
-        {
-            if (value == null)
-                value = string.Empty;
-
-            if (value.Length < field.Length)
-            {
-                FieldAlignment alignment = field.Alignment ?? Options.DefaultAlignment;
-                char padCharacter = field.PadCharacter ?? Options.DefaultPadCharacter;
-                if (alignment == FieldAlignment.Left)
-                    value = value.PadRight(field.Length, padCharacter);
-                else // FieldAlignment.Right
-                    value = value.PadLeft(field.Length, padCharacter);
-            }
-            else if (value.Length > field.Length)
-            {
-                if (Options.ThrowOverflowException)
-                    throw new FixedWidthOverflowException(value, field.Length);
-#if !NETSTANDARD2_0
-                value = value[..field.Length];
-#else
-                value = value.Substring(0, field.Length);
-#endif
-            }
-            return value;
-        }
-
-        /// <summary>
-        /// Clears all buffers and causes any unbuffered data to be written to the underlying stream.
-        /// </summary>
-        public void Flush() => Writer.Flush();
-
-        /// <summary>
-        /// Closes the current <see cref="FixedWidthWriter"/> object and the underlying stream.
-        /// </summary>
-        public void Close() => Writer.Close();
-
-        /// <summary>
-        /// Releases resources used by this <see cref="FixedWidthWriter"/> object.
-        /// </summary>
-        public void Dispose()
-        {
-            GC.SuppressFinalize(this);
-            Writer.Dispose();
+            DefaultAlignment = FieldAlignment.Left;
+            DefaultPadCharacter = ' ';
+            TrimFields = true;
+            ThrowDataException = true;
+            ThrowOutOfRangeException = true;
+            ThrowOverflowException = true;
+            NewLine = Environment.NewLine;
         }
     }
 }
